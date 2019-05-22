c4e03f8699c851cf60cab99e0d318aa44740c62c
rigaya <rigaya34589@live.jp>
2019-05-22 22:43:14 +0900

    最初のコミット。

---
+++ Readme.md
@@ -0,0 +1,6 @@
+
+# hdr10plus_gen
+
+cmake projectの練習用。
+
+[x265](https://bitbucket.org/multicoreware/x265/wiki/Home)のlibhdr10plusを使用して、jsonファイルを展開する。
---
+++ hdr10plus_gen/hdr10plus_gen.cpp
@@ -0,0 +1,71 @@
+﻿
+#include <fcntl.h>
+#include <io.h>
+#include <cstdio>
+#include <cstdint>
+#include <memory>
+#include <iostream>
+#include <string>
+#include "hdr10plus.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+    std::string inputFile;
+    std::string outputFile;
+    for (int iarg = 1; iarg < argc; iarg++) {
+        if (std::string(argv[iarg]) == "-i") {
+            iarg++;
+            inputFile = argv[iarg];
+        } else if (std::string(argv[iarg]) == "-o") {
+            iarg++;
+            outputFile = argv[iarg];
+        }
+    }
+    if (inputFile.length() == 0) {
+        std::cerr << "input file not specified!" << std::endl;
+        return 1;
+    }
+    if (outputFile.length() == 0) {
+        outputFile = inputFile + ".bin";
+    }
+
+    if (outputFile == "-") {
+        if (_setmode(_fileno(stdout), _O_BINARY) == 1) {
+            std::cerr << "failed to switch stdout to binary mode." << std::endl;
+            return 1;
+        }
+    }
+    unique_ptr<FILE, decltype(&fclose)> fp(outputFile == "-" ? stdout : fopen(outputFile.c_str(), "wb"), fclose);
+
+    const auto hdr10plus = hdr10plus_api_get();
+    if (hdr10plus == nullptr) {
+        std::cerr << "failed to get hdr10plus api!" << std::endl;
+        return 1;
+    }
+
+    uint8_t **cim;
+    const int32_t cimNum = hdr10plus->hdr10plus_json_to_movie_cim(inputFile.c_str(), cim);
+
+    for (int32_t icim = 0; icim < cimNum; icim++) {
+        int i = 0;
+        int32_t dataSize = 0;
+        while (cim[icim][i] == 0xFF) {
+            dataSize += cim[icim][i++];
+        }
+        dataSize += cim[icim][i++];
+        if (   fwrite(&icim,     sizeof(icim),     1, fp.get()) != 1
+            || fwrite(&dataSize, sizeof(dataSize), 1, fp.get()) != 1) {
+            return 1;
+        }
+        if (dataSize > 0) {
+            if (fwrite(&cim[icim][i], 1, dataSize, fp.get()) != dataSize) {
+                return 1;
+            }
+            fflush(fp.get());
+        }
+        std::cerr << "cim[" << icim << "] size: " << dataSize << std::endl;
+    }
+    fp.reset();
+    return 0;
+}
